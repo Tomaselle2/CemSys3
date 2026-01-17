ed7f887b89a6d651bc17190f62ffae26c74ca054
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2026-01-16 21:24:50 -0300

    Se mojora la animacion de carga de pantalla

--- wwwroot/js/HelpersJS.js
+++ wwwroot/js/HelpersJS.js
@@ -1,10 +1,34 @@
-﻿document.addEventListener("submit", async function (e) {
+﻿//PARA ANIMACION DE PANTALLA DE CARGA
+window.Loader = {
+    show() {
+        document.getElementById("global-loader")?.classList.remove("d-none");
+    },
+    hide() {
+        document.getElementById("global-loader")?.classList.add("d-none");
+    }
+};
+
+// Antes de navegar (links normales)
+document.addEventListener("click", function (e) {
+    const link = e.target.closest("a");
+
+    if (!link) return;
+    if (link.target === "_blank") return;
+    if (link.hasAttribute("data-no-loader")) return;
+    if (link.getAttribute("href")?.startsWith("#")) return;
+
+    Loader.show();
+});
+
+//PARA LOS FORMUALRIOS
+document.addEventListener("submit", async function (e) {
 
     const form = e.target;
     const submitter = e.submitter;
 
     if (!submitter) return;
 
+   
     //Formularios con confirmación
     if (form.classList.contains("js-confirm")) {
 
@@ -18,11 +42,28 @@
 
         if (!confirmado) return; // cancelar → no bloquea nada
 
+        Loader.show();
         AlertService.blockButton(submitter);
         form.submit();
         return;
     }
 
+
     //Formularios normales
+    Loader.show();
     AlertService.blockButton(submitter);
 });
+
+////FUNCIONES PARA TABLAS (ANIMACION)
+//function showTableLoaderFromElement(element) {
+//    const container = element.closest(".table-container");
+//    container?.querySelector(".table-loader")?.classList.remove("d-none");
+//}
+
+//document.addEventListener("submit", function (e) {
+//    const form = e.target;
+
+//    if (!form.closest(".table-container")) return;
+
+//    showTableLoaderFromElement(form);
+//});
