6a1807469a1a5a907d698c049cf99f818cbf2060
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2026-02-14 12:38:38 -0300

    Se orientan las imagenes

--- Helpers/PDF/PdfHelper.cs
+++ Helpers/PDF/PdfHelper.cs
@@ -1,9 +1,10 @@
-﻿using System.Reflection.Metadata;
-using iText.IO.Image;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
-using static System.Net.Mime.MediaTypeNames;
+using iText.IO.Image;
 
 namespace CemSys3.Helpers.PDF
 {
@@ -11,14 +12,30 @@ namespace CemSys3.Helpers.PDF
     {
         public static byte[] ImagenComoPdf(byte[] imagenBytes)
         {
+            //1) Corregir orientación EXIF automáticamente
+            using var image = SixLabors.ImageSharp.Image.Load(imagenBytes);
+
+            image.Mutate(x => x.AutoOrient());
+
+            using var correctedStream = new MemoryStream();
+
+            image.Save(correctedStream, new JpegEncoder
+            {
+                Quality = 90
+            });
+
+            var imagenCorregida = correctedStream.ToArray();
+
+            //2) Crear PDF
             using var ms = new MemoryStream();
             using var writer = new PdfWriter(ms);
             using var pdf = new PdfDocument(writer);
-            var document = new iText.Layout.Document(pdf);
+            var document = new Document(pdf);
 
-            var imageData = ImageDataFactory.Create(imagenBytes);
+            var imageData = ImageDataFactory.Create(imagenCorregida);
             var img = new iText.Layout.Element.Image(imageData);
-            img.SetAutoScale(true); // Ajusta sin distorsionar
+
+            img.SetAutoScale(true);
 
             document.Add(img);
             document.Close();
