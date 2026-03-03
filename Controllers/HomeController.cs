370ae885dd503f58b7d5dae92dffd486e766767f
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2026-03-03 20:54:27 -0300

    Notificacion de notas pendientes al inicial la app

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
+using CemSys3.DTOs.SweetAlert;
 using CemSys3.Enumerables;
 using CemSys3.Helpers.Roles_Autenticacion;
 using CemSys3.Interfaces.Notificaciones;
 using CemSys3.Models;
+using CemSys3.ViewModels.Home;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -17,10 +19,33 @@ namespace CemSys3.Controllers
         }
 
         [AuthorizeRole(RolUsuario.Administrador, RolUsuario.Empleado)]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            
-            return View();
+            NotificacionHomeVM viewModel = new NotificacionHomeVM();
+
+            try
+            {
+                viewModel.NotificacionNota = await _notificacionesService.NotificacionNotasPendientes();
+                if (viewModel.NotificacionNota.CantidadNotasIngresoPendientes != 0 || viewModel.NotificacionNota.CantidadNotasRecordatorioPendientes != 0)
+                {
+                    viewModel.SweetAlert = new SweetAlertDTO
+                    {
+                        Titulo = "¡Tienes notas pendientes!",
+                        Mensaje = $"Tienes {viewModel.NotificacionNota.CantidadNotasIngresoPendientes} notas de ingreso pendientes y {viewModel.NotificacionNota.CantidadNotasRecordatorioPendientes} notas de recordatorio pendientes.",
+                        Tipo = "warning"
+                    };
+                }
+            }
+            catch (Exception ex) {
+                viewModel.SweetAlert = new SweetAlertDTO
+                {
+                    Titulo = "Error al cargar las notificaciones",
+                    Mensaje = ex.Message,
+                    Tipo = "error"
+                };
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ ViewModels/Home/NotificacionHomeVM.cs
@@ -0,0 +1,13 @@
+﻿using CemSys3.DTOs.Nota;
+using CemSys3.DTOs.SweetAlert;
+
+namespace CemSys3.ViewModels.Home
+{
+    public class NotificacionHomeVM
+    {
+        public NotificacionNotaDTO NotificacionNota { get; set; } = new NotificacionNotaDTO();
+
+        //manero de errores
+        public SweetAlertDTO? SweetAlert { get; set; }
+    }
+}
