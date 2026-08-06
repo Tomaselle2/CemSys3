e3d11f4d5dc378d3e12676464781d4768211df23
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2026-08-06 18:41:29 -0300

    Se realiza carga masiva de datos de panteones

--- Helpers/ScriptDB/Cemsys3_insert.sql
+++ Helpers/ScriptDB/Cemsys3_insert.sql
@@ -732,37 +732,3 @@ SELECT 0.00, fila.n, @conceptoNicho, anio.id, NULL, 1
 FROM (VALUES (1),(2),(3),(4),(5)) AS fila(n)
 CROSS JOIN (SELECT id FROM AnioConcesion WHERE anios IN (5,10,15,25)) AS anio;
 
---Job para pasar de concesion vigente a vencida 
---BEGIN TRANSACTION;
-
----- 1. Obtener trámites que pasan a vencido
---DECLARE @TramitesVencidos TABLE (
---    tramiteId INT
---);
-
---INSERT INTO @TramitesVencidos (tramiteId)
---SELECT t.id
---FROM Tramites t
---INNER JOIN Concesiones c ON c.tramiteId = t.id
---WHERE 
---    c.vencimiento < GETDATE()
---    AND t.estadoActualId = 6; -- Vigente
-
-
----- 2. Actualizar estado actual
---UPDATE t
---SET estadoActualId = 7 -- Vencido
---FROM Tramites t
---INNER JOIN @TramitesVencidos tv ON tv.tramiteId = t.id;
-
-
----- 3. Insertar historial
---INSERT INTO HistorialEstadoTramite (fecha, tramiteId, estadoTramiteId)
---SELECT 
---    GETDATE(),
---    tramiteId,
---    7 -- Vencido
---FROM @TramitesVencidos;
-
-
---COMMIT;
